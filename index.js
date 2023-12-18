99895b81a516cef07c1973bb24ba7f6e3a2ec307
Waqar Hassan <waqar_hassan@selinc.com>
2023-12-19 01:03:13 +0400

    dmas api done.

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ import {
   divfunction,
 } from "./functions.js";
 
-let x = addfunction(3, 4);
-//console.log(`result is: ${x}`);
-console.log(req);
+for (let i = 0; i < req.operations.length; i++) {
+  if (req.operations[i] === "addition") {
+    let x = addfunction(req.firstNumber, req.secondNumber);
+    console.log("result for addition is: ", x);
+  } else if (req.operations[i] === "multiplication") {
+    let x = mulfunction(req.firstNumber, req.secondNumber);
+    console.log("result for multiplication is: ", x);
+  } else if (req.operations[i] === "subtraction") {
+    let x = subfunction(req.firstNumber, req.secondNumber);
+    console.log("resul for subtraction ist: ", x);
+  } else if (req.operations[i] === "division") {
+    let x = divfunction(req.firstNumber, req.secondNumber);
+    console.log("result for division: ", x);
+  } else {
+    console.log("error....");
+  }
+}
